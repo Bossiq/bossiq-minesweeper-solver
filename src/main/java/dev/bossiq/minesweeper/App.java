2a3172c4a6388949614d0d81372255c710513866
Bossiq <marian.oboroceanu2@gmail.com>
2025-10-12 05:03:09 +0200

    Added backend check ( App )
    CONSOLE ONLY backend check in App

--- src/main/java/dev/bossiq/minesweeper/App.java
+++ src/main/java/dev/bossiq/minesweeper/App.java
@@ -13,6 +13,21 @@ public class App extends Application {
         StackPane root = new StackPane(new Label("Hello Gamer !"));
         stage.setScene(new Scene(root, 800, 600));
         stage.show();
+
+        //TEMP
+        
+        dev.bossiq.minesweeper.model.Board b = new dev.bossiq.minesweeper.model.Board(9, 9, 10);
+        System.out.println("Created 9x9 with 10 mines");
+
+        dev.bossiq.minesweeper.model.Board.RevealResult r = b.reveal(0, 0);
+        System.out.println("Hit mine? " + r.hitMine + " changed=" + r.revealed.size());
+
+        b.toggleFlag(1, 1);
+        b.toggleFlag(1, 1);
+
+        System.out.println(b.snapshotStats());
+
+        //TEMP
     }
     public static void main(String[] args) {
         launch(args);
